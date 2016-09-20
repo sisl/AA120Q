3d82fc4709e2a8c60dbc54f6501ea923f234045b
Tim Wheeler <wheelert@stanford.edu>
2016-09-20 12:45:28 -0700

    adding lectures and tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,13 @@
 using Base.Test
 using AA120Q
+using NBInclude
+
+lecture_dir = Pkg.dir("AA120Q", "lectures")
+for d in readdir(lecture_dir)
+    if endswith(d, ".ipynb")
+        fullpath = joinpath(lecture_dir, d)
+        stuff = "using NBInclude; nbinclude(\"" * fullpath * "\")"
+        cmd = `julia -e $stuff`
+        run(cmd)
+    end
+end
