ba2d924f881d3caef53406db4a6596422ab2ba35
Tim Wheeler <wheelert@stanford.edu>
2017-01-10 20:33:16 -0800

    removing data files

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using Base.Test
 using AA120Q
 using NBInclude
 
-lecture_dir = Pkg.dir("AA120Q", "lectures")
-for d in readdir(lecture_dir)
-    if endswith(d, ".ipynb")
-        fullpath = joinpath(lecture_dir, d)
-        stuff = "using NBInclude; nbinclude(\"" * fullpath * "\")"
-        cmd = `julia -e $stuff`
-        run(cmd)
-    end
-end
+# lecture_dir = Pkg.dir("AA120Q", "lectures")
+# for d in readdir(lecture_dir)
+#     if endswith(d, ".ipynb")
+#         fullpath = joinpath(lecture_dir, d)
+#         stuff = "using NBInclude; nbinclude(\"" * fullpath * "\")"
+#         cmd = `julia -e $stuff`
+#         run(cmd)
+#     end
+# end
