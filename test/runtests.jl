738b00dbb969476a96f09bb7ed4e3089034093f0
Mykel Kochenderfer <mykel@stanford.edu>
2018-12-31 17:12:58 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Test
 using AA120Q
 using NBInclude
+using InteractiveUtils
+using Plots
 
 lecture_dir = joinpath(dirname(pathof(AA120Q)), "..", "lectures")
 for d in readdir(lecture_dir)
     if endswith(d, ".ipynb")
         fullpath = joinpath(lecture_dir, d)
-        stuff = "using Interact, Reactive, NBInclude; nbinclude(\"" * fullpath * "\")"
-        cmd = `julia -e $stuff`
-        run(cmd)
+        @nbinclude(fullpath; softscope=true)
     end
 end
