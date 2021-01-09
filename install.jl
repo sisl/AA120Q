a027bbbcca1329ceb094f133c8c699cc69fe6da2
Robert Moss <rmoss92@gmail.com>
2021-01-08 20:13:23 -0800

    Local dev of AA120Q repo in install.jl

--- install.jl
+++ install.jl
@@ -32,7 +32,7 @@ Pkg.add("TikzPictures")
 
 Pkg.build()
 
-Pkg.add(PackageSpec(name="AA120Q", url="https://github.com/sisl/AA120Q.git"))
+pkg"dev ."
 Pkg.build("AA120Q")
 
 @info "Packages installed."
