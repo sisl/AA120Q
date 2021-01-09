b13d1b40084ecee3b389191f62effbd54133039e
Sydney Katz <sydney.m.katz@wustl.edu>
2021-01-08 16:14:59 -0800

    cd to assignments before testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using AA120Q
 # using Plots
 
 assignment_dir = joinpath(dirname(pathof(AA120Q)), "..", "assignments")
+cd("../assignments")
 for d in readdir(assignment_dir)
     if startswith(d, "0") && endswith(d, ".jl")
         fullpath = joinpath(assignment_dir, d)
